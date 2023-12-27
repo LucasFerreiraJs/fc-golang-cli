e6fdf71859346b656a121761df5dce26dbb3befc
Lucas Ferreira <casferreiraa@gmail.com>
2023-12-27 01:52:43 -0300

    feat(category): create category with flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,34 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
 
+	"github.com/lucasferreirajs/16-cli/internal/database"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 )
 
+func GetDb() *sql.DB {
 
+	db, err := sql.Open("sqlite3", "./data.db")
+	fmt.Println("criando db")
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func GetCategoryDB(db *sql.DB) database.Category {
+
+	return *database.NewCategory(db)
+
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -47,5 +65,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
